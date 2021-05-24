1c79b38c740fb964e5ec446e0a4b20f542610064
Ryujin <69662328+BoyDownTown@users.noreply.github.com>
2021-05-24 20:28:58 +1000

    Update perform.py

--- perform/perform.py
+++ perform/perform.py
@@ -12,7 +12,7 @@ class Perform(commands.Cog):
 
     async def req(self, url):
         async with aiohttp.ClientSession() as session:
-            res = async with session.get(f"https://nekos.life/api/v2/img/{url}")
+            res = async with session.get(f'https://nekos.life/api/v2/img/{url}')
             res = await res.json()
             return box.Box(res)  
     
