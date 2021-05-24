8df4f081e18b94905d13a39dcaef14cd12e0865c
Ryujin <69662328+BoyDownTown@users.noreply.github.com>
2021-05-24 20:36:46 +1000

    Update perform.py

--- perform/perform.py
+++ perform/perform.py
@@ -12,21 +12,7 @@ class Perform(commands.Cog):
         self.bot = bot
         
     def box(text: str, lang: str = "") -> str:
-    """Get the given text in a code block.
 
-    Parameters
-    ----------
-    text : str
-        The text to be marked up.
-    lang : `str`, optional
-        The syntax highlighting language for the codeblock.
-
-    Returns
-    -------
-    str
-        The marked up text.
-
-    """
     ret = "```{}\n{}\n```".format(lang, text)
     return ret
 
