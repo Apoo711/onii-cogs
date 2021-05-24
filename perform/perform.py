7770bf9b48844f27d24530021f7ba2cad7f328b4
Ryujin <69662328+BoyDownTown@users.noreply.github.com>
2021-05-24 20:35:25 +1000

    Update perform.py

--- perform/perform.py
+++ perform/perform.py
@@ -1,14 +1,34 @@
 import discord
 import aiohttp
 import asyncio
+import box
 
 from redbot.core import commands 
-from redbot.core.utils import chat_formatting, box
+from redbot.core.utils import chat_formatting
 
 class Perform(commands.Cog):
     """Perform different actions, like cuddle, poke etc."""
     def __init__(self, bot):
         self.bot = bot
+        
+    def box(text: str, lang: str = "") -> str:
+    """Get the given text in a code block.
+
+    Parameters
+    ----------
+    text : str
+        The text to be marked up.
+    lang : `str`, optional
+        The syntax highlighting language for the codeblock.
+
+    Returns
+    -------
+    str
+        The marked up text.
+
+    """
+    ret = "```{}\n{}\n```".format(lang, text)
+    return ret
 
     async def req(self, url):
         async with aiohttp.ClientSession() as session:
