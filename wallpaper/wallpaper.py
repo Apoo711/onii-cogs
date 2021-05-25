d703646e0177b4864befa6a6c0a9d60254cb2cdd
Ryujin <69662328+BoyDownTown@users.noreply.github.com>
2021-05-25 20:30:18 +1000

    Update wallpaper.py

--- wallpaper/wallpaper.py
+++ wallpaper/wallpaper.py
@@ -85,4 +85,20 @@ class Wallpaper(commands.Cog):
          )
          embed.set_image(url=res['url'])
          await ctx.reply(embed=embed)
-        
+    
+    @commands.command()
+    async def fanart(self, ctx):
+        r = requests.get("https://www.gensh.in/gallery/wallpaper")
+        soup = bs(r.content, "html.parser")
+        imgs = soup.findAll("a", class_ = "col-sm-12 col-md-6 col-lg-4 col-xl-3")
+        embed = discord.Embed(
+            colour = csettings.embedcolour(),
+            timestamp = ctx.message.created_at
+        )
+
+        chosen = random.choice(imgs)
+        url = "https://www.gensh.in" + chosen["href"]
+        embed.set_image(url = url)
+        embed.set_author(name = "Genshin Impact Fan Art", icon_url = self.client.user.avatar_url)
+        embed.set_footer(text = f"Requested by: {ctx.author}", icon_url = self.client.user.avatar_url)
+        await ctx.send(embed = embed)
