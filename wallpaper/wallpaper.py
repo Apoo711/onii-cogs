50a8e74cd8e122a919fa0b2546e69a03a6b8f4cb
Ryujin <69662328+BoyDownTown@users.noreply.github.com>
2021-05-29 22:19:06 +1000

    Update wallpaper.py

--- wallpaper/wallpaper.py
+++ wallpaper/wallpaper.py
@@ -106,7 +106,7 @@ class Wallpaper(commands.Cog):
          res = await r.json()
          em = discord.Embed(color=ctx.author.color, title="Neko")
          em.set_footer(text=f"Requested by: {str(ctx.author)} | Powered by nekos.life", icon_url=ctx.author.avatar_url)
-         em.set_image(url=res.url)
+         em.set_image(url=res['url'])
          await ctx.send(embed=em)
     
     @commands.command()
