469d2293d9e46e17999c61be7915c4c2220a7aba
Ryujin <69662328+BoyDownTown@users.noreply.github.com>
2021-06-01 16:05:23 +1000

    Update perform.py

--- perform/perform.py
+++ perform/perform.py
@@ -86,7 +86,7 @@ class Perform(commands.Cog):
          res = await r.json()
          em = discord.Embed(
           colour=discord.Colour.random(), 
-          title=f"**{ctx.author.name}** just tickled {'**{str(user.name)}**' if user else 'themselves'}!",
+          title=f"**{ctx.author.name}** just tickled {f'**{str(user.name)}**' if user else 'themselves'}!",
 	 )
          em.set_footer(text=f"Requested by: {str(ctx.author)} | Powered by shiro.gg", icon_url=ctx.author.avatar_url)
          em.set_image(url=res['url'])
