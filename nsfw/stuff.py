0aad56fdc4943a190f45ea46184d954834470b25
Ryujin <69662328+BoyDownTown@users.noreply.github.com>
2021-06-02 12:19:47 +1000

    Create stuff.py

---
+++ nsfw/stuff.py
@@ -0,0 +1,30 @@
+import discord
+
+from redbot.core.bot import Red
+from redbot.core import commands
+from redbot.core.i18n import Translator, cog_i18n
+from redbot.core.utils.chat_formatting import bold, box, inline
+
+_ = Translator("hentai", __file__)
+
+
+@cog_i18n(_)
+class stuff(commands.Cog):
+  
+    __author__ = ["Onii-chan"]
+    __version__ = "0.1.0"
+
+    def format_help_for_context(self, ctx: commands.Context) -> str:
+        """Thanks Sinbad!"""
+        pre_processed = super().format_help_for_context(ctx)
+        return f"{pre_processed}\n\nAuthors: {', '.join(self.__author__)}\nCog Version: {self.__version__}"
+        
+    async def _version_msg(self, ctx: commands.Context, version: str, authors: List[str]):
+        """Cog version message."""
+        msg = box(
+            _("Nsfw cog version: {version}\nAuthors: {authors}").format(
+                version=version, authors=", ".join(authors)
+            ),
+            lang="py",
+        )
+        return await ctx.send(msg)
