90cd4843661f89ac53cea73a3d65f7caaa54fb34
Onii-chan <69662328+Onii-Chan-Discord@users.noreply.github.com>
2021-06-20 19:44:22 +1000

    Add Oniitools cog!
    The cog is finally here!

---
+++ oniitools/oniitools.py
@@ -0,0 +1,17 @@
+import discord
+import random 
+
+from redbot.core import commands
+
+class Oniitools(commands.Cog):
+    """A random assortment of fun commands!"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def penis(self, user : discord.Member):
+        """Detects user's penis length this is 100% accurate."""
+        random.seed(user.id)
+        p = "8" + "="*random.randint(0, 30) + "D"
+        await self.bot.say("Size: " + p)
