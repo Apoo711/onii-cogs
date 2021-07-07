0a39ec82d83a07fb4a099097b330cf3ea8f006c6
Onii-chan <69662328+Onii-Chan-Discord@users.noreply.github.com>
2021-07-08 09:37:30 +1000

    Update image.py

--- image/image.py
+++ image/image.py
@@ -383,3 +383,40 @@ class Image(commands.Cog):
             icon_url=ctx.message.author.avatar_url,
         )
         await ctx.reply(embed=embed, mention_author=False)
+        
+    @other.command()
+    @commands.guild_only()
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    async def max(self, ctx: commands.Context):
+        """Shows some space images from reddit.
+        Images shown are taken from r/spaceengine and r/LandscapeAstro.
+        """
+        SPACE = "spaceengine", "LandscapeAstro"
+        SPACE_CHOOSER = random.choice(SPACE)
+        API = f"https://www.reddit.com/r/{SPACE_CHOOSER}/top.json?sort=new"
+        async with ctx.typing():
+            await asyncio.sleep(1)
+        async with aiohttp.ClientSession() as session:
+            async with session.get(API) as resp:
+                data = await resp.json()
+                data = data["data"]
+                children = data["children"]
+                post = random.choice(children)["data"]
+                title = post["title"]
+                url = post["url_overridden_by_dest"]
+                link = post["permalink"]
+                subreddit_name = post["subreddit_name_prefixed"]
+                r_author = post["author"]
+                upvote = post["ups"]
+
+        embed = discord.Embed(
+            title=title,
+            colour=discord.Colour.random(),
+            url=f"https://reddit.com{link}",
+        )
+        embed.set_image(url=url)
+        embed.set_footer(
+            text=f"👍 {upvote} | Post by {r_author} | {subreddit_name}",
+            icon_url=ctx.message.author.avatar_url,
+        )
+        await ctx.reply(embed=embed, mention_author=False)
