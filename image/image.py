aae2c53fade15c241719e141b7bc4402441d4ee9
Onii-Chan-Discord <aryan18guptaggs@gmail.com>
2021-08-01 13:07:46 +1000

    A test cmd (DONT USE)

--- image/image.py
+++ image/image.py
@@ -795,3 +795,80 @@ class Image(commands.Cog):
             embed=embed,
             mention_author=False,
         )
+
+    @commands.command()
+    @commands.guild_only()
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    async def test(self, ctx: commands.Context):
+        """Shows some moe images from reddit.
+        Images shown are taken from:
+        r/awwnime, r/animeboys, r/cuteanimeboys and r/CuteAnimeGirls.
+        """
+        await ctx.trigger_typing()
+        SUBREDDITS = [
+            "animeboys",
+            "CuteAnimeGirlss",
+            "cuteanimeboys",
+            "awwnime"
+        ]
+        API = random.choice(SUBREDDITS)
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                        f"https://api.martinebot.com/v1/images/subreddit?name={API}"
+                    ) as resp:
+                origin = await resp.json()
+                data = origin["data"]
+                url = data["image_url"]
+                subreddit = data["subreddit"] or ""
+                sub_name = subreddit["name"] or "Unknown"
+                sub_url = subreddit["url"] or ""
+                author = data["author"] or ""
+                r_author = author["name"] or "Unknown"
+                r_author_url = author["url"] or ""
+                title = data["title"] or ""
+                created_at = data["created_at"] or ""
+                downvotes = data["downvotes"] or ""
+                comments = data["comments"] or ""
+                ups = data["upvotes"] or ""
+                link = data["post_url"] or ""
+
+                if data["nsfw"] and not ctx.channel.is_nsfw():
+                    return await ctx.send(
+                        "Sorry the contents of this post are NSFW and this channel isn't set to allow NSFW content, please it on and try again later."
+                    )
+
+        embed = discord.Embed(
+            title="Here's a random image...:frame_photo:",
+            colour=discord.Colour.random(),
+            description=(
+                "**Post by:** [u/{}]({})\n"
+                "**From:** [r/{}]({})\n"
+                "**This post was created on:** <t:{}:F>\n"
+                "**Title:** [{}]({})"
+            ).format(
+                r_author,
+                r_author_url,
+                sub_name,
+                sub_url,
+                created_at,
+                title,
+                link,
+            ),
+        )
+        embed.set_image(url=url)
+        embed.set_footer(
+            text="👍  {} • 👎  {} • 💬  {} • martinebot.com API".format(
+                ups,
+                downvotes,
+                comments,
+            ),
+            icon_url=ctx.message.author.avatar_url,
+        )
+
+        try:
+            await ctx.reply(
+                embed=embed,
+                mention_author=False,
+            )
+        except discord.HTTPException:
+            await ctx.send("Something went wrong while posting an image.")
