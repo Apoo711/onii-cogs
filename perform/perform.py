15c15bcc857639020a4e16e7140aad88b777ed14
sravan <57430307+sravan1946@users.noreply.github.com>
2021-09-22 09:11:40 +0530

    add the api stuff

--- perform/perform.py
+++ perform/perform.py
@@ -15,11 +15,10 @@ limitations under the License.
 """
 
 
-from random import randint
-
 import aiohttp
 import discord
-from redbot.core import Config, commands
+from redbot.core import commands, Config
+from random import randint
 
 
 async def api_call(call_uri, returnObj=False):
@@ -49,20 +48,6 @@ class Perform(commands.Cog):
         self.bot = bot
         self.config = Config.get_conf(self, identifier=8423644625413)
         default_global = {
-            "highfive": [
-                "https://media1.tenor.com/images/0ae4995e4eb27e427454526c05b2e3dd/tenor.gif?itemid=12376992",
-                "https://media1.tenor.com/images/7b1f06eac73c36721912edcaacddf666/tenor.gif?itemid=10559431",
-                "https://media1.tenor.com/images/c3263b8196afc25ddc1d53a4224347cd/tenor.gif?itemid=9443275",
-                "https://media1.tenor.com/images/56d6725009312574e4798c732cebc5fe/tenor.gif?itemid=12312526",
-                "https://media1.tenor.com/images/e96d2396570a2fadd9c83e284a1ca675/tenor.gif?itemid=5390726",
-                "https://media1.tenor.com/images/106c8e64e864230341b59cc892b5a980/tenor.gif?itemid=5682921",
-                "https://media1.tenor.com/images/b714d7680f8b49d69b07bc2f1e052e72/tenor.gif?itemid=13400356",
-                "https://media1.tenor.com/images/0c23b320822afd5b1ce3faf01c2b9b69/tenor.gif?itemid=14137078",
-                "https://media1.tenor.com/images/e2f299d05a7b1832314ec7a331440d4e/tenor.gif?itemid=5374033",
-                "https://media1.tenor.com/images/16267f3a34efb42598bd822effaccd11/tenor.gif?itemid=14137081",
-                "https://media1.tenor.com/images/9730876547cb3939388cf79b8a641da9/tenor.gif?itemid=8073516",
-                "https://media1.tenor.com/images/ce85a2843f52309b85515f56a0a49d06/tenor.gif?itemid=14137077",
-            ],
             "feed": [
                 "https://media1.tenor.com/images/93c4833dbcfd5be9401afbda220066ee/tenor.gif?itemid=11223742",
                 "https://media1.tenor.com/images/33cfd292d4ef5e2dc533ff73a102c2e6/tenor.gif?itemid=12165913",
@@ -78,50 +63,17 @@ class Perform(commands.Cog):
             ],
             "spank":[
                 "https://media1.tenor.com/images/ef5f040254c2fbf91232088b91fe2341/tenor.gif?itemid=13569259",
-                "https://media1.tenor.com/images/d0f32f61c2964999b344c6846b30e1d6/tenor.gif?itemid=13665166",
-                "https://media1.tenor.com/images/b51750728709353206263f0407f0be96/tenor.gif?itemid=16173937",
-                "https://media1.tenor.com/images/1235505dc4abd822a7f540ad00e24a17/tenor.gif?itemid=15788982",
-                "https://media1.tenor.com/images/996159e911ae816c153bfa523e18d7c4/tenor.gif?itemid=17299734",
+                "https://media1.tenor.com/images/fa2472b2cca1e4a407b7772b329eafb4/tenor.gif?itemid=21468457",
                 "https://media1.tenor.com/images/2eb222b142f24be14ea2da5c84a92b08/tenor.gif?itemid=15905904",
-                "https://media1.tenor.com/images/c9b1fa86a103a3bf878ab1741d0bfdfe/tenor.gif?itemid=19349444",
-                "https://media1.tenor.com/images/a7ed1e6575b047ae219c8bdef3cdb799/tenor.gif?itemid=16082139",
-                "https://media1.tenor.com/images/0d202a5b98b413a88a4611feae8cd855/tenor.gif?itemid=16910479f",
-                "https://media1.tenor.com/images/5250cae3a499250ee32627109c4cec0b/tenor.gif?itemid=19431854",
-                "https://media1.tenor.com/images/02e7e815066e728e721a44eb3e1118d3/tenor.gif?itemid=16055441",
-                "https://media1.tenor.com/images/d7d63622652099676fd345d1ad0589b7/tenor.gif?itemid=18714058",
-                "https://media1.tenor.com/images/051be15e85edb4cf90e639b986ad861d/tenor.gif?itemid=17314327",
-                "https://media1.tenor.com/images/64a98f91d57957204c7e57b374912af0/tenor.gif?itemid=21371415",
-                "https://media1.tenor.com/images/693afd5812160c00a1fa8582de15a83e/tenor.gif?itemid=5458569",
                 "https://media1.tenor.com/images/86b5a47d495c0e8c5c3a085641a91aa4/tenor.gif?itemid=15964704",
-                "https://media1.tenor.com/images/6f086e496d3e2650603caa21cc22046d/tenor.gif?itemid=18105116",
-                "https://media1.tenor.com/images/4587f9a7c71519cb86fcdda746c434ed/tenor.gif?itemid=9583397",
-                "https://media1.tenor.com/images/cbb47488c390edbddfe299d18d2909ae/tenor.gif?itemid=12178033"
-            ],
-            "facepalm":[
-                "https://media1.tenor.com/images/bc3f3842afb1edcba095f9bf766405b2/tenor.gif?itemid=17778269",
-                "https://media1.tenor.com/images/76d2ec47ec76fa36b2fce913331ba7e3/tenor.gif?itemid=5533025",
-                "https://media1.tenor.com/images/be96db9b9acfd04fd2f5d890e2c51781/tenor.gif?itemid=14355381",
-                "https://media1.tenor.com/images/5e29a1db9149211728b22bfd01f88771/tenor.gif?itemid=10336271",
-                "https://media1.tenor.com/images/2e69f243490dedfdfc15c4a9aa52364c/tenor.gif?itemid=15580787",
-                "https://media1.tenor.com/images/480cdeb59d3d5d50dd206283a944b8e1/tenor.gif?itemid=16327659",
-                "https://media1.tenor.com/images/1aa0aa009ad5176e9bec54fe0e784323/tenor.gif?itemid=10405345",
-                "https://media1.tenor.com/images/fce5aa9f4825a2adabfc9c91686167bc/tenor.gif?itemid=16842960",
-                "https://media1.tenor.com/images/5b378a7894bc420954616a61eeb7f8c7/tenor.gif?itemid=21256762",
-                "https://media1.tenor.com/images/04ce28c62c8cfeb102b3ac2a9bf28050/tenor.gif?itemid=12411417",
-                "https://media1.tenor.com/images/2b8e5a43989d6fb81776267d1efba103/tenor.gif?itemid=21833649",
-                "https://media1.tenor.com/images/5bbe44f124365864b1537f686d0a77f5/tenor.gif?itemid=20556956"
-            ],
-            "kill":[
-                "https://media1.tenor.com/images/55507aea306782b916659085fc062909/tenor.gif?itemid=8932977",
-                "https://media1.tenor.com/images/2c945adbbc31699861f411f86ce8058f/tenor.gif?itemid=5459053",
-                "https://media1.tenor.com/images/c3cbe5b795cd40c0b51d02711f6e3978/tenor.gif?itemid=17223062",
-                "https://media1.tenor.com/images/28c19622e8d7362ccc140bb24e4089ec/tenor.gif?itemid=9363668",
-                "https://media1.tenor.com/images/a80b2bf31635899ac0900ea6281a41f6/tenor.gif?itemid=5535365",
-                "https://media1.tenor.com/images/15cd532af605deec12a680f7e6d17181/tenor.gif?itemid=13961185",
-                "https://media1.tenor.com/images/e4db2e0888c2c85a042ea9e54fc4d771/tenor.gif?itemid=16079109",
-                "https://media1.tenor.com/images/2706b52a7bf7b34cfe43d7f49381ee85/tenor.gif?itemid=13617665",
-                "https://media1.tenor.com/images/b73f2fb18d2dcc2ea7fa0ac20830f91f/tenor.gif?itemid=11409825",
-                "https://media1.tenor.com/images/9df97f1c83c532cc29ab8d9ec099acf5/tenor.gif?itemid=10616092"
+                "https://media1.tenor.com/images/31d58e53313dc9bbd6435d824d2a5933/tenor.gif?itemid=11756736",
+                "https://media1.tenor.com/images/97624764cb41414ad2c60d2028c19394/tenor.gif?itemid=16739345",
+                "https://media1.tenor.com/images/f21c5c56e36ce0dfcdfe7c7993578c46/tenor.gif?itemid=21371415",
+                "https://media1.tenor.com/images/58f5dcc2123fc73e8fb6b76f149441bc/tenor.gif?itemid=12086277",
+                "https://media1.tenor.com/images/eafb13b900645ddf3b30cf9cc28e9f91/tenor.gif?itemid=4603671",
+                "https://media1.tenor.com/images/be2bb9db1c8b8dc2194ec6a1b3d96b89/tenor.gif?itemid=18811244",
+                "https://media.giphy.com/media/OoCuLoM6iEhYk/giphy.gif",
+                "https://media.giphy.com/media/Qo3qovmbqaKT6/giphy.gif"
             ],
         }
         self.config.register_global(**default_global)
@@ -413,29 +365,6 @@ class Perform(commands.Cog):
                 await ctx.reply(embed=em, mention_author=False)
 
     @commands.cooldown(1, 10, commands.BucketType.user)
-    @commands.command(name="highfive")
-    @commands.bot_has_permissions(embed_links=True)
-    async def highfive(self, ctx, user: discord.Member):
-        """Highfives a user!"""
-
-        author = ctx.message.author
-        images = await self.config.highfive()
-
-        mn = len(images)
-        i = randint(0, mn - 1)
-
-        em = discord.Embed(
-            colour=discord.Colour.random(),
-            description=f"**{ctx.author.mention}** highfives {f'**{str(user.mention)}**' if user else 'themselves'}!",
-        )
-        em.set_footer(
-            text=f"Requested by: {str(ctx.author)}",
-            icon_url=ctx.author.avatar_url,
-        )
-        em.set_image(url=images[i])
-        await ctx.reply(embed=em, mention_author=False)
-
-    @commands.cooldown(1, 10, commands.BucketType.user)
     @commands.command(name="feed")
     @commands.bot_has_permissions(embed_links=True)
     async def feed(self, ctx, user: discord.Member):
@@ -625,8 +554,8 @@ class Perform(commands.Cog):
 
     @commands.cooldown(1, 10, commands.BucketType.user)
     @commands.command(name="facepalm")
-    @commands.bot_has_permissions(embed_links=True)
-    async def facepalm(self, ctx):
+    @commands.guild_only()
+    async def faceplam(self, ctx):
         """Do a facepalm!"""
         api_key = (await self.bot.get_shared_api_tokens("perform")).get("api_key")
         if not api_key:
@@ -665,9 +594,9 @@ class Perform(commands.Cog):
             color=discord.Colour.random(),
         )
 
-        em = discord.Embed(
-            colour=discord.Colour.random(),
-            description=f"**{ctx.author.mention}** is facepalming!",
+        embed.set_footer(
+            text=f"Requested by {ctx.message.author.display_name}",
+            icon_url=ctx.message.author.avatar_url,
         )
         embed.set_author(
                 name=self.bot.user.display_name,
@@ -679,12 +608,11 @@ class Perform(commands.Cog):
                 "https://kawaii.red/api/gif/facedesk/token=" + api_key
             )
         )
-        em.set_image(url=images[i])
-        await ctx.reply(embed=em, mention_author=False)
+        await ctx.reply(embed=embed, mention_author=False)
 
     @commands.cooldown(1, 10, commands.BucketType.user)
-    @commands.command(name="kill")
-    @commands.bot_has_permissions(embed_links=True)
+    @commands.command()
+    @commands.guild_only()
     async def kill(self, ctx, user: discord.Member):
         """Kill a user!"""
         api_key = (await self.bot.get_shared_api_tokens("perform")).get("api_key")
@@ -754,8 +682,14 @@ class Perform(commands.Cog):
             color=discord.Colour.random(),
         )
 
-        author = ctx.message.author
-        images = await self.config.kill()
+        embed.set_footer(
+            text=f"Requested by {ctx.message.author.display_name}",
+            icon_url=ctx.message.author.avatar_url,
+        )
+        embed.set_author(
+                name=self.bot.user.display_name,
+                icon_url=self.bot.user.avatar_url
+            )
 
         embed.set_image(
             url=await api_call2(
@@ -776,9 +710,10 @@ class Perform(commands.Cog):
             description=f"**{ctx.author.mention}** is laughing!",
             color=discord.Colour.random(),
         )
-        em.set_footer(
-            text=f"Requested by: {str(ctx.author)}",
-            icon_url=ctx.author.avatar_url,
+
+        embed.set_footer(
+            text=f"Requested by {ctx.message.author.display_name}",
+            icon_url=ctx.message.author.avatar_url,
         )
         embed.set_author(
                 name=self.bot.user.display_name,
