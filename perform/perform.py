36f9c5b914a6cebe6ca81900e88a72cba039c3c6
sravan <57430307+sravan1946@users.noreply.github.com>
2022-04-16 16:16:05 +0530

    [perform] set author for these too

--- perform/perform.py
+++ perform/perform.py
@@ -397,6 +397,7 @@ class Perform(commands.Cog):
             colour=discord.Colour.random(),
             description=f"**{ctx.author.mention}** just spanked {f'**{str(user.mention)}**' if user else 'themselves'}!",
         )
+        em.set_author(name=self.bot.user.display_name, icon_url=self.bot.user.avatar_url)
         em.set_image(url=images[i])
         target = await self.config.custom("Target", ctx.author.id, user.id).spank_r()
         used = await self.config.user(ctx.author).spank_s()
@@ -469,6 +470,7 @@ class Perform(commands.Cog):
             colour=discord.Colour.random(),
             description=f"**{ctx.author.mention}** feeds {f'**{str(user.mention)}**' if user else 'themselves'}!",
         )
+        em.set_author(name=self.bot.user.display_name, icon_url=self.bot.user.avatar_url)
         em.set_image(url=images[i])
         target = await self.config.custom("Target", ctx.author.id, user.id).feed_r()
         used = await self.config.user(ctx.author).feed_s()
